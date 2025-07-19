0d399642cbd64823b4ad66d2db2348150411f0df
reham0000 <amsarkerreham@gmail.com>
2025-07-19 20:11:18 +0600

    added students enrolled page

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import AddCourse from "./pages/educator/AddCourse";
 import MyCourses from "./pages/educator/MyCourses";
 import StudentsEnrolled from "./pages/educator/StudentsEnrolled";
 import Navbar from "./components/student/Navbar";
+import "quill/dist/quill.snow.css";
+// import 'react-quill/dist/quill.snow.css';
 
 const App = () => {
 
--- src/pages/educator/AddCourse.jsx
+++ src/pages/educator/AddCourse.jsx
@@ -1,11 +1,91 @@
-import React from 'react';
+import Quill from "quill";
+import uniqid from "uniqid";
+import React, { useEffect, useRef, useState } from "react";
+import { assets } from "../../assets/assets";
 
 const AddCourse = () => {
-    return (
-        <div>
-            <h1>Add Course Page</h1>
+  const quillRef = useRef(null);
+  const editorRef = useRef(null);
+
+  const [courseTitle, setCourseTitle] = useState("");
+  const [coursePrice, setCoursePrice] = useState(0);
+  const [discount, setDiscount] = useState(0);
+  const [image, setImage] = useState(null);
+  const [chapters, setChapters] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
+  const [currentChapterId, setCurrentChapterId] = useState(null);
+
+  const [lectureDetails, setLectureDetails] = useState({
+    lectureTitle: "",
+    lectureDuration: "",
+    lectureUrl: "",
+    isPreviewFree: false,
+  });
+
+  useEffect(() => {
+    // initiate quill only once
+    if (!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current, {
+        theme: "snow",
+      });
+    }
+  }, []);
+
+  return (
+    <div className="h-screen overflow-scroll flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0">
+      <form className="flex flex-col gap-4 max-w-md w-full text-gray-500">
+        <div className="flex flex-col gap-1">
+          <p>Course Title</p>
+          <input
+            onChange={(e) => setCourseTitle(e.target.value)}
+            value={courseTitle}
+            placeholder="Type here"
+            type="text"
+            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500"
+            required
+          />
         </div>
-    );
+        <div className="flex flex-col gap-1">
+          <p>Course Description</p>
+          <div ref={editorRef}></div>
+        </div>
+        <div className="flex items-center justify-between flex-wrap">
+          <div className="flex flex-col gap-1">
+            <input
+              onChange={(e) => setCoursePrice(e.target.value)}
+              value={coursePrice}
+              type="number"
+              placeholder="0"
+              className="outline-none md:py-2.5 pl-2 py-2 w-28 rounded border border-gray-500"
+              required
+            />
+          </div>
+          <div className="flex md:flex-row flex-col items-center gap-3">
+            <p>Course Thumbnail</p>
+            <label htmlFor="thumbnailImage" className="flex items-center gap-3">
+              <img
+                src={assets.file_upload_icon}
+                className="p-3 bg-blue-500 rounded"
+                alt=""
+              />
+              <input
+                type="file"
+                id="thumbnailImage"
+                onChange={(e) => setImage(e.target.files[0])}
+                accept="image/*"
+                hidden
+              />
+              <img
+                src={image ? URL.createObjectURL(image) : ""}
+                className="max-h-10"
+                alt=""
+              />
+            </label>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddCourse;
