d0805ced49d7916ae51e556b2a4d20c9b080320f
Al Mobin Reham <amsarkerreham@gmail.com>
2025-08-02 22:06:41 +0600

    add logo3

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
             onClick={() => openSignIn()}
             className="bg-blue-600 text-white px-5 py-2 rounded-full"
           >
-            Create Accounts
+            Create Account
           </button>
         )}
       </div>
